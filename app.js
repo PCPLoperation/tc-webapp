0d9a050bc4dcaf15e6e09e13dd3e82c6fce22d91
PCPLoperation <operations@polybond.in>
2025-12-17 19:33:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let products = [];
 
-fetch("https://drive.google.com/uc?export=download&id1Zuy6uAIjeAwqlQUhlJKoVm5HYqOXC8lI")
+fetch("https://drive.google.com/uc?id=1Zuy6uAIjeAwqlQUhlJKoVm5HYqOXC8lI&export=media")
   .then(response => response.json())
   .then(data => {
     products = data;
